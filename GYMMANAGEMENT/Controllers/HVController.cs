46b3f0b24d0a9ee7f814a91dbd2f3cec52f6edc1
ttiendat <ttiendat@LAPTOP-5CPV61CC>
2024-10-31 13:08:57 +0700

    Add project files.

---
+++ GYMMANAGEMENT/Controllers/HVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GYMMANAGEMENT.Controllers
+{
+    public class HVController : Controller
+    {
+        // GET: HV
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
